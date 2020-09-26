f5cdf40fb3f3e0031d8f555dc7096bfaaed9ef78
shoaiburrehman <shoaiburrehman249@gmail.com>
2020-09-26 10:57:32 +0500

    Updates

---
+++ precache-manifest.2369f2055afc258e771303f5a742ab5b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2415db663b3ef8b4028afb7b58327fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5345fcfe2f50a5d835c0",
+    "url": "/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "c00bb1ed8c50cf434daf",
+    "url": "/static/css/main.cf976a2b.chunk.css"
+  },
+  {
+    "revision": "5345fcfe2f50a5d835c0",
+    "url": "/static/js/2.b751c772.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/static/js/2.b751c772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00bb1ed8c50cf434daf",
+    "url": "/static/js/main.e564f403.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "124647daa54719edff86d43fc7a980f5",
+    "url": "/static/media/Dog-vs-Cat.124647da.jpg"
+  },
+  {
+    "revision": "7bd4d2aa7723bb364b0ecb5ee0606ec1",
+    "url": "/static/media/Salary2.7bd4d2aa.png"
+  },
+  {
+    "revision": "6f6406af0dd84b4988d14549e5e6bfdc",
+    "url": "/static/media/airbnbPrice.6f6406af.jpg"
+  },
+  {
+    "revision": "760d32f8adbeaa737f478e658969909c",
+    "url": "/static/media/amazonSentiment.760d32f8.jpg"
+  },
+  {
+    "revision": "b626825a1402f5bc11b3a4a3d54828c8",
+    "url": "/static/media/creditRisk.b626825a.jpg"
+  },
+  {
+    "revision": "a4598633c712b4618065fd74b3593d3e",
+    "url": "/static/media/futureSales.a4598633.png"
+  },
+  {
+    "revision": "e13e7e0fd9249ed5a2dba4fc1fd977e9",
+    "url": "/static/media/graduation.e13e7e0f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc7ee83e5653970c9486b2eb2dc62ca2",
+    "url": "/static/media/movieRecommendation.bc7ee83e.jpg"
+  },
+  {
+    "revision": "412ef65414b73c453bc014623240f59c",
+    "url": "/static/media/neural.412ef654.jpg"
+  },
+  {
+    "revision": "bcab92db3abb9d80ee92960e1d2de362",
+    "url": "/static/media/portfolio.bcab92db.png"
+  },
+  {
+    "revision": "ba080107fb4035fb5455956a45cf2584",
+    "url": "/static/media/profile.ba080107.png"
+  },
+  {
+    "revision": "61e23185f53a3da15c6c7e0b4bf92dce",
+    "url": "/static/media/titanic.61e23185.jpg"
+  },
+  {
+    "revision": "a70dfae94ea0636bd9b5da722ba050fe",
+    "url": "/static/media/twitterSentiment2.a70dfae9.jpg"
+  }
+]);
